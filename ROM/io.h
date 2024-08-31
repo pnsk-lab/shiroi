c45c7b3ea61d31838c1a354fce1c4c85fcf5762c
Nishi <nishi@nishi.boats>
2024-08-31 11:58:03 +0000

    fix io call: https://www.msx.org/forum/msx-talk/development/sdcc-4112-a-game-changer-for-c-programming
    git-svn-id: file:///raid/svn-personal/shiroi/trunk@23 162db6bc-b981-7948-bc20-66b78829fa50

--- ROM/io.h
+++ ROM/io.h
@@ -3,7 +3,7 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-unsigned char inp(unsigned char);
-void outp(unsigned char, unsigned char);
+unsigned char inp(unsigned char) __sdcccall(0);
+void outp(unsigned char, unsigned char) __sdcccall(0);
 
 #endif
