45e28c2fbed38646d6bba84981295e68568351c4
Nishi <nishi@nishi.boats>
2024-08-31 12:29:19 +0000

    fixed it in better way
    git-svn-id: file:///raid/svn-personal/shiroi/trunk@25 162db6bc-b981-7948-bc20-66b78829fa50

--- ROM/io.h
+++ ROM/io.h
@@ -3,7 +3,7 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-#if !((SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR == 1 && SDCC_VERSION_PATCH == 11) || (SDCC_VERSION_MAJOR > 4) || (SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR > 1) || (SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR == 1 && SDCC_VERSION_PATCH > 11))
+#if __SDCCCALL == 0
 #define __sdcccall(x)
 #endif
 
