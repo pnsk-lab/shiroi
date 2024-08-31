82a007c32303ad1f023de139504cf73df7d747b5
Nishi <nishi@nishi.boats>
2024-08-31 12:24:15 +0000

    workaround for pre 4.1.12 sdcc
    git-svn-id: file:///raid/svn-personal/shiroi/trunk@24 162db6bc-b981-7948-bc20-66b78829fa50

--- ROM/io.h
+++ ROM/io.h
@@ -3,6 +3,10 @@
 #ifndef __IO_H__
 #define __IO_H__
 
+#if !((SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR == 1 && SDCC_VERSION_PATCH == 11) || (SDCC_VERSION_MAJOR > 4) || (SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR > 1) || (SDCC_VERSION_MAJOR == 4 && SDCC_VERSION_MINOR == 1 && SDCC_VERSION_PATCH > 11))
+#define __sdcccall(x)
+#endif
+
 unsigned char inp(unsigned char) __sdcccall(0);
 void outp(unsigned char, unsigned char) __sdcccall(0);
 
